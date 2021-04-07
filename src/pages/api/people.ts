fb3a5294f6796664894af30e0485f25991df2043
yuikim <nokdoo00@gmail.com>
2021-04-06 22:35:59 -0400

    add NextApiHandler to check error

--- src/pages/api/people.ts
+++ src/pages/api/people.ts
@@ -1,9 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import sqlite from 'sqlite';
 
-export default async function getPeople(req: NextApiRequest, res: NextApiResponse) {
+const authenticated = (fn: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
+  return await fn(req, res)
+}
+
+export default authenticated(async function getPeople(req: NextApiRequest, res: NextApiResponse) {
   const db = await sqlite.open('./mydb.sqlite');
   const people = await db.all('select id, email, name from Person')
 
   res.json(people);
-}
+})
