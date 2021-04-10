f8c1dfa5a66b345459c5bc75c13afe271b180be0
yuikim <nokdoo00@gmail.com>
2021-04-10 08:36:43 -0400

    add headers to send cookie for server side rendering

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -1,12 +1,17 @@
 import { NextPageContext } from 'next';
-import React from 'react';
+import fetch from 'isomorphic-unfetch';
 
 export default function People({ people }: any) {
   return <div>People: {JSON.stringify(people)}</div>;
 }
 
 People.getInitialProps = async (ctx: NextPageContext) => {
-  const resp = await fetch('http://localhost:3000/api/people');
+  const cookie = ctx.req?.headers.cookie;
+  const resp = await fetch('http://localhost:3000/api/people', {
+    headers: {
+      cookie: cookie!,
+    },
+  });
   const json = await resp.json();
   return { people: json };
 };
